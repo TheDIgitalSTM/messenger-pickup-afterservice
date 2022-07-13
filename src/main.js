0572fe257d3ebf36b1ff4f0cb7acc02b7eec11b4
Nakarin Jaiseengam <nakarinnat1993@gmail.com>
2022-07-13 12:21:08 +0700

    Restore code

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-// import './assets/index.css';
+import Vue from 'vue'
+import App from './App.vue'
+import './assets/index.css';
 
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
 
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
-import { createApp } from "vue";
-import App from "./App.vue";
-import './styles/app.css'; // Here
-import store from './store'
-import router from './router'
-import vClickOutside from "click-outside-vue3"
-
-const app = createApp(App);
-app.use(router)
-app.mount('#app')
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
